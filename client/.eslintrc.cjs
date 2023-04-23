bbb3333b67dcfa6b746136603fbebc1c873243d5
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-04-23 22:46:35 +0300

    set up vite for client

---
+++ client/.eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['react-refresh', 'react-hooks', '@typescript-eslint', 'jsx-a11y', 'prettier'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+  },
+};
