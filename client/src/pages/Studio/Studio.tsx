b6e2c6591e05aa4e1ddbab6e07d618daf3851b3b
mykola <mykola@emil.de>
2023-04-26 19:31:02 +0200

    client: add navbar

--- client/src/pages/Studio/Studio.tsx
+++ client/src/pages/Studio/Studio.tsx
@@ -1,28 +1,32 @@
 import { Tabs, TabList, TabPanels, Tab, TabPanel, Box } from '@chakra-ui/react';
+import Navbar from "../Navbar/Navbar";
 
 const Studio = () => {
   return (
-    <Box h="100vh" w="100%">
-      <Tabs orientation="vertical" variant="solid-rounded" colorScheme="gray" h="100%">
-        <TabList>
-          <Tab>One</Tab>
-          <Tab>Two</Tab>
-          <Tab>Three</Tab>
-        </TabList>
+      <div>
+        <Navbar/>
+        <Box h="100vh" w="100%">
+          <Tabs orientation="vertical" variant="solid-rounded" colorScheme="gray" h="100%">
+            <TabList>
+              <Tab>One</Tab>
+              <Tab>Two</Tab>
+              <Tab>Three</Tab>
+            </TabList>
 
-        <TabPanels maxW="350px" bgColor="gray">
-          <TabPanel>
-            <p>one!</p>
-          </TabPanel>
-          <TabPanel>
-            <p>two!</p>
-          </TabPanel>
-          <TabPanel>
-            <p>three!</p>
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
-    </Box>
+            <TabPanels maxW="350px" bgColor="gray">
+              <TabPanel>
+                <p>one!</p>
+              </TabPanel>
+              <TabPanel>
+                <p>two!</p>
+              </TabPanel>
+              <TabPanel>
+                <p>three!</p>
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+        </Box>
+      </div>
   );
 };
 
