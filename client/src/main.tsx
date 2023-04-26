586e3980a996d8d420fd9e303c22075837e097c1
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-04-26 15:51:37 +0300

    set up chakra provider and react router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
-import './index.css';
+import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <StrictMode>
+    <ChakraProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
+  </StrictMode>,
 );
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,11 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
   resolve: {
     alias: {
       '~': path.resolve(__dirname, './src'),
