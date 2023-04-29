dd2ffceb26b0f102c1122a733d739d9e3f107c56
Polina Balazhy <lefugdot@gmail.com>
2023-04-29 12:27:16 +0300

    fix the overflow of the infinite scroller (#19)

--- client/src/pages/Studio/tools/Images/InfiniteWrapper.tsx
+++ client/src/pages/Studio/tools/Images/InfiniteWrapper.tsx
@@ -13,6 +13,7 @@ const InfiniteWrapper = ({ count, fetchItems, children }: Props) => {
     <InfiniteScroll
       scrollableTarget={'imageGrid'}
       dataLength={count}
+      style={{ overflow: 'hidden' }}
       next={fetchItems}
       hasMore={true}
       loader={<Loader />}
