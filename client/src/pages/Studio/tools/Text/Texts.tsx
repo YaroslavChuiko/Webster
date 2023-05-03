8f115888137b305b81379844a1474f9cc5ed4e6f
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-05-03 21:37:31 +0300

    add buttons for adding text with default styles

--- client/src/pages/Studio/tools/Text/Texts.tsx
+++ client/src/pages/Studio/tools/Text/Texts.tsx
@@ -1,20 +1,46 @@
 import { Button, VStack } from '@chakra-ui/react';
+// import { useEffect } from 'react';
 import { DEFAULT_TEXT_OBJECT } from '~/consts/stage-object';
 import useStageObject from '~/hooks/use-stage-object';
+import { StageTextData } from '~/types/stage-object';
+
+const headingStyles: Partial<StageTextData> = {
+  text: 'Add a heading',
+  fontSize: 78,
+  width: 600,
+  fontStyle: 'bold',
+};
+
+const subheadingStyles: Partial<StageTextData> = {
+  text: 'Add a subheading',
+  fontSize: 44,
+  width: 400,
+  fontStyle: 'bold',
+};
+
+const bodyTextStyles: Partial<StageTextData> = {
+  text: 'Add a little bit of body text',
+  fontSize: 30,
+  width: 400,
+  fontStyle: 'normal',
+};
 
 const Texts = () => {
   const { createOne } = useStageObject();
 
-  const addTextToStage = () => {
+  const addTextToStage = (options: Partial<StageTextData> = {}) => {
     createOne({
-      text: 'Click to resize. Double click to edit.',
       ...DEFAULT_TEXT_OBJECT,
+      ...options,
     });
   };
 
   return (
     <VStack>
-      <Button onClick={addTextToStage}>Add Text</Button>
+      <Button onClick={() => addTextToStage()}>Add a text box</Button>
+      <Button onClick={() => addTextToStage(headingStyles)}>Add a heading</Button>
+      <Button onClick={() => addTextToStage(subheadingStyles)}>Add a subheading</Button>
+      <Button onClick={() => addTextToStage(bodyTextStyles)}>Add a little bit of body text</Button>
     </VStack>
   );
 };
