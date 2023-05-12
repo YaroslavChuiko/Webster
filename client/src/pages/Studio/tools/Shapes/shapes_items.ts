92f07097c0e9a0f0e962e17dda5270d5c053fa79
Svetlana Khomenko <hsvetlana03@gmail.com>
2023-05-12 20:18:56 +0300

    add images for shapes adding

--- client/src/pages/Studio/tools/Shapes/shapes_items.ts
+++ client/src/pages/Studio/tools/Shapes/shapes_items.ts
@@ -6,48 +6,58 @@ export type shapeItemType = {
   shapeType: ShapeType;
 };
 
+const SHAPES_FOLDER_PATH = '~/../public/shapes';
+
 export const shapesItems = [
   {
+    src: `${SHAPES_FOLDER_PATH}/square.png`,
     name: 'Square',
     shapeType: ShapeType.RECT,
     strokeScaleEnabled: false,
   },
   {
+    src: `${SHAPES_FOLDER_PATH}/circle.png`,
     name: 'Circle',
     shapeType: ShapeType.CIRCLE,
     strokeScaleEnabled: false,
   },
   {
+    src: `${SHAPES_FOLDER_PATH}/trianle.png`,
     name: 'Trianle',
     shapeType: ShapeType.POLYGON,
     sides: 3,
     strokeScaleEnabled: false,
   },
   {
+    src: `${SHAPES_FOLDER_PATH}/pentagon.png`,
     name: 'Pentagon',
     shapeType: ShapeType.POLYGON,
     sides: 5,
     strokeScaleEnabled: false,
   },
   {
+    src: `${SHAPES_FOLDER_PATH}/hexagon.png`,
     name: 'Hexagon',
     shapeType: ShapeType.POLYGON,
     sides: 6,
     strokeScaleEnabled: false,
   },
   {
+    src: `${SHAPES_FOLDER_PATH}/heptagon.png`,
     name: 'Heptagon',
     shapeType: ShapeType.POLYGON,
     sides: 7,
     strokeScaleEnabled: false,
   },
   {
+    src: `${SHAPES_FOLDER_PATH}/octagon.png`,
     name: 'Octagon',
     shapeType: ShapeType.POLYGON,
     sides: 8,
     strokeScaleEnabled: false,
   },
   {
+    src: `${SHAPES_FOLDER_PATH}/star-4.png`,
     name: 'Star 4',
     shapeType: ShapeType.STAR,
     numPoints: 4,
@@ -56,6 +66,7 @@ export const shapesItems = [
     strokeScaleEnabled: false,
   },
   {
+    src: `${SHAPES_FOLDER_PATH}/star-5.png`,
     name: 'Star 5',
     shapeType: ShapeType.STAR,
     numPoints: 5,
@@ -64,6 +75,7 @@ export const shapesItems = [
     strokeScaleEnabled: false,
   },
   {
+    src: `${SHAPES_FOLDER_PATH}/star-6.png`,
     name: 'Star 6',
     shapeType: ShapeType.STAR,
     numPoints: 6,
@@ -72,6 +84,7 @@ export const shapesItems = [
     strokeScaleEnabled: false,
   },
   {
+    src: `${SHAPES_FOLDER_PATH}/arrow.png`,
     name: 'Arrow',
     shapeType: ShapeType.ARROW,
     points: [0, 0, 50, 0],
