a1a684a2ad149be08c0ae050eec7674609058b96
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-05-14 22:04:21 +0300

    refactor

--- client/src/pages/Studio/EditingToolbar/TextEditing/SpacingSettingsMenu/LetterSpacingSettings.tsx
+++ client/src/pages/Studio/EditingToolbar/TextEditing/SpacingSettingsMenu/LetterSpacingSettings.tsx
@@ -1,66 +1,29 @@
-import {
-  Flex,
-  NumberInput,
-  NumberInputField,
-  Slider,
-  SliderFilledTrack,
-  SliderMark,
-  SliderThumb,
-  SliderTrack,
-  Text,
-  VStack,
-} from '@chakra-ui/react';
 import useStageObject from '~/hooks/use-stage-object';
+import CustomNumberInput from './CustomNumberInput';
 
 type Props = {
   id: string;
   letterSpacing: number;
 };
 
-const minVal = -50;
-const maxVal = 250;
-const step = 1;
-
 const LetterSpacingSettings = ({ id, letterSpacing }: Props) => {
   const { updateOne } = useStageObject();
 
-  const handleInputChange = (valueAsString: string, valueAsNumber: number) => {
-    const regex = /^-?\d{1,3}$/;
-
-    if (regex.test(valueAsString) && valueAsNumber >= minVal && valueAsNumber <= maxVal) {
-      updateOne({ id, data: { letterSpacing: valueAsNumber } });
-    }
-  };
-
-  const handleSliderChange = (value: number) => {
+  const handleChange = (value: number) => {
     updateOne({ id, data: { letterSpacing: value } });
   };
 
   return (
-    <VStack pb="10px">
-      <Flex w="100%" justify="space-between">
-        <Text as="span">Letter spacing</Text>
-        <NumberInput size="sm" maxW="40px" min={minVal} max={maxVal} value={letterSpacing} onChange={handleInputChange}>
-          <NumberInputField px="5px" textAlign="center" />
-        </NumberInput>
-      </Flex>
-      <Slider
-        focusThumbOnChange={false}
-        min={minVal}
-        max={maxVal}
-        step={step}
-        value={letterSpacing}
-        onChange={handleSliderChange}
-      >
-        <SliderMark value={0} fontSize="sm">
-          0
-        </SliderMark>
-        <SliderTrack>
-          <SliderFilledTrack />
-        </SliderTrack>
-        <SliderThumb boxSize="15px" />
-      </Slider>
-    </VStack>
+    <CustomNumberInput
+      min={-15}
+      max={250}
+      step={1}
+      label="Letter spacing"
+      mark={0}
+      value={letterSpacing}
+      pattern={/^-?\d{1,3}$/}
+      onChange={handleChange}
+    />
   );
 };
 
--- client/src/pages/Studio/EditingToolbar/TextEditing/SpacingSettingsMenu/SpacingSettingsMenu.tsx
+++ client/src/pages/Studio/EditingToolbar/TextEditing/SpacingSettingsMenu/SpacingSettingsMenu.tsx
@@ -1,6 +1,6 @@
 import { Button, Menu, MenuButton, MenuList, Tooltip } from '@chakra-ui/react';
-import useStageObject from '~/hooks/use-stage-object';
-import CustomNumberInput from './CustomNumberInput';
+import LetterSpacingSettings from './LetterSpacingSettings';
+import LineSpacingSettings from './LineSpacingSetting';
 
 type Props = {
   id: string;
@@ -9,16 +9,6 @@ type Props = {
 };
 
 const SpacingSettingsMenu = ({ id, letterSpacing, lineHeight }: Props) => {
-  const { updateOne } = useStageObject();
-
-  const onLetterSpacingChange = (value: number) => {
-    updateOne({ id, data: { letterSpacing: value } });
-  };
-
-  const onLineSpacingChange = (value: number) => {
-    updateOne({ id, data: { lineHeight: value } });
-  };
-
   return (
     <Menu>
       <Tooltip hasArrow label="Spacing" placement="bottom" openDelay={500}>
@@ -27,26 +17,8 @@ const SpacingSettingsMenu = ({ id, letterSpacing, lineHeight }: Props) => {
         </MenuButton>
       </Tooltip>
       <MenuList p="15px 30px" minW="300px">
-        <CustomNumberInput
-          min={-50}
-          max={250}
-          step={1}
-          label="Letter spacing"
-          mark={0}
-          value={letterSpacing}
-          pattern={/^-?\d{1,3}$/}
-          onChange={onLetterSpacingChange}
-        />
-        <CustomNumberInput
-          min={0.5}
-          max={2.5}
-          step={0.01}
-          label="Line spacing"
-          mark={1.2}
-          value={lineHeight}
-          pattern={/^\d\.?\d{1,2}$/}
-          onChange={onLineSpacingChange}
-        />
+        <LetterSpacingSettings id={id} letterSpacing={letterSpacing} />
+        <LineSpacingSettings id={id} lineHeight={lineHeight} />
       </MenuList>
     </Menu>
   );
