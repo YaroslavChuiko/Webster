5dea24070e21bf09a6698110a33ec45131c17625
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-05-24 15:03:30 +0300

    update Navbar styles

--- client/src/pages/Navbar/Navbar.tsx
+++ client/src/pages/Navbar/Navbar.tsx
@@ -1,47 +1,33 @@
-import { Box, Flex, Spacer, Button, Text, useColorModeValue, ChakraProvider } from '@chakra-ui/react';
+import { Box, Button, Flex, Icon, Spacer } from '@chakra-ui/react';
+import { HiChevronLeft } from 'react-icons/hi';
 import { Link } from 'react-router-dom';
-import { extendTheme } from '@chakra-ui/react';
-import { ChevronLeftIcon } from '@chakra-ui/icons';
-function Navbar() {
-  const customTheme = extendTheme({
-    components: {
-      Button: {
-        variants: {
-          'navbar-transparent': {
-            color: 'white',
-            bg: 'transparent',
-            _hover: {
-              bg: useColorModeValue('teal.300', 'teal.400'),
-            },
-          },
-        },
-      },
-    },
-  });
 
+function Navbar() {
   return (
-    <ChakraProvider theme={customTheme}>
-      <Flex
-        bgGradient="linear(to-r, teal.300, blue.300, blue.400, blue.500, blue.500, purple.600, purple.600, purple.600)"
-        pt="2"
-        pb="2"
-        align="center"
-        id="navbar"
-      >
-        <Box>
-          <Button colorScheme="blue" mr="4" ml="1" as={Link as never} to="/home" variant="navbar-transparent">
-            <ChevronLeftIcon w={8} h={8} />
-            <Text ml="2">Home</Text>
-          </Button>
-        </Box>
-        <Spacer />
-        <Box>
-          <Button colorScheme="blue" mr="4" as={Link as never} to="/auth/sign-in">
-            Login
-          </Button>
-        </Box>
-      </Flex>
-    </ChakraProvider>
+    <Flex bgGradient="linear(to-r, pink.500, purple.500)" py="2" align="center" id="navbar">
+      <Box>
+        <Button
+          leftIcon={<Icon as={HiChevronLeft} boxSize={6} />}
+          mr="4"
+          ml="1"
+          as={Link}
+          to="/home"
+          color="white"
+          bgColor="transparent"
+          _hover={{
+            bg: 'pink.400',
+          }}
+        >
+          Home
+        </Button>
+      </Box>
+      <Spacer />
+      <Box>
+        <Button colorScheme="gray" mr="4" as={Link} to="/auth/sign-in">
+          Login
+        </Button>
+      </Box>
+    </Flex>
   );
 }
 
