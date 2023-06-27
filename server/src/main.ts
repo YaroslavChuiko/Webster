b2dc567d080dbf3e1cf6eeff76f3f997866822c8
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-06-27 20:15:54 +0300

    set host

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,14 +5,14 @@ import {
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { LoggerService } from './modules/logger/logger.service';
-// import { appConfig } from './shared/configs/app.config';
+import { appConfig } from './shared/configs/app.config';
 import { fastifyHelmet } from '@fastify/helmet';
 import { PrismaService } from './shared/services/prisma.service';
 import { ValidationPipe } from './shared/pipes';
 import { AllExceptionsFilter } from './shared/filters';
 
-// const port = appConfig.getPort();
-const port = process.env.PORT || 8080;
+const port = appConfig.getPort();
+// const port = process.env.PORT || 8080;
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
@@ -45,7 +45,7 @@ async function bootstrap() {
   });
 
   await app
-    .listen(port)
+    .listen(port, '0.0.0.0')
     .then(() => new LoggerService().info(`server listening at port ${port}`));
 }
 
