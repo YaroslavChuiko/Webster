2df2a63c640e60b663c54ebb64335587d76c21a2
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-07-03 13:36:17 +0300

    fix unused variable

--- client/src/store/api/baseQuery.ts
+++ client/src/store/api/baseQuery.ts
@@ -1,6 +1,6 @@
 import { fetchBaseQuery } from '@reduxjs/toolkit/dist/query';
-import type { BaseQueryFn, FetchArgs, FetchBaseQueryError } from '@reduxjs/toolkit/query';
-import { logout, login } from '../slices/auth-slice';
+// import type { BaseQueryFn, FetchArgs, FetchBaseQueryError } from '@reduxjs/toolkit/query';
+// import { logout, login } from '../slices/auth-slice';
 import type { RootState } from '../store';
 
 const baseQuery = fetchBaseQuery({
@@ -15,43 +15,43 @@ const baseQuery = fetchBaseQuery({
   credentials: 'include',
 });
 
-const baseQueryWithReauth: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError> = async (
-  args,
-  api,
-  extraOptions,
-) => {
-  let result = await baseQuery(args, api, extraOptions);
+// const baseQueryWithReauth: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError> = async (
+//   args,
+//   api,
+//   extraOptions,
+// ) => {
+//   let result = await baseQuery(args, api, extraOptions);
 
-  if (result.error && result.error.status === 401) {
-    const refreshResult = await baseQuery(
-      {
-        url: 'auth/refresh',
-        method: 'POST',
-      },
-      api,
-      extraOptions,
-    );
-    if (refreshResult.data) {
-      api.dispatch(login(refreshResult.data));
-      result = await baseQuery(args, api, extraOptions);
-    } else {
-      api.dispatch(logout());
-    }
-  }
+//   if (result.error && result.error.status === 401) {
+//     const refreshResult = await baseQuery(
+//       {
+//         url: 'auth/refresh',
+//         method: 'POST',
+//       },
+//       api,
+//       extraOptions,
+//     );
+//     if (refreshResult.data) {
+//       api.dispatch(login(refreshResult.data));
+//       result = await baseQuery(args, api, extraOptions);
+//     } else {
+//       api.dispatch(logout());
+//     }
+//   }
 
-  if (result.error && result.error.status === 500) {
-    return {
-      ...result,
-      error: {
-        ...result.error,
-        data: {
-          message: 'A server error occurred.',
-        },
-      },
-    };
-  }
+//   if (result.error && result.error.status === 500) {
+//     return {
+//       ...result,
+//       error: {
+//         ...result.error,
+//         data: {
+//           message: 'A server error occurred.',
+//         },
+//       },
+//     };
+//   }
 
-  return result;
-};
+//   return result;
+// };
 
 export default baseQuery;
