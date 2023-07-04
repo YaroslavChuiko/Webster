ba04d4616282b0573a133468f1dabd4ff2c5d499
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-07-04 14:08:25 +0300

    fix fonts loading

--- client/src/styles/theme/index.ts
+++ client/src/styles/theme/index.ts
@@ -2,8 +2,8 @@ import { extendTheme } from '@chakra-ui/react';
 
 const theme = extendTheme({
   fonts: {
-    heading: `"Inter Variable", sans-serif`,
-    body: `"Inter Variable", sans-serif`,
+    heading: `"Inter", sans-serif`,
+    body: `"Inter", sans-serif`,
   },
   semanticTokens: {
     colors: {
